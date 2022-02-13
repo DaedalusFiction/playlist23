56295eeb92a1e9cabda90616a2683eda7ead51c8
David Sorensen <teardownthismall@gmail.com>
2022-02-13 12:05:33 -0500

    styled progressbar, song controls

--- client/src/components/MusicPlayer.js
+++ client/src/components/MusicPlayer.js
@@ -1,5 +1,5 @@
 import SongUploader from "./SongUploader";
-import { useEffect, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { socket } from "../socket/socket";
 import styles from "../styles/rangeInput.css";
 import { useParams } from "react-router-dom";
@@ -11,18 +11,28 @@ const MusicPlayer = ({
     playingMusic,
     setPlayingMusic,
 }) => {
-    const player = useRef();
+    const [songDuration, setSongDuration] = useState(0);
+    const [songCurrentTime, setSongCurrentTime] = useState(0);
+
+    const player = useRef(); //references audio player
+    const progressBar = useRef(); //references progress bar
+    const animationRef = useRef();
+
     const params = useParams();
+
     useEffect(() => {
         //set up listeners
         socket.on("playSong", (song) => {
             setNowPlaying(song.song);
             player.current.play();
+
             setPlayingMusic(true);
+            animationRef.current = requestAnimationFrame(animateProgressBar);
         });
         socket.on("pauseSong", () => {
             player.current.pause();
             setPlayingMusic(false);
+            cancelAnimationFrame(animationRef.current);
         });
 
         return () => {
@@ -32,10 +42,25 @@ const MusicPlayer = ({
         };
     }, [setNowPlaying, setPlayingMusic]);
 
+    useEffect(() => {
+        if (playingMusic) {
+            setSongCurrentTime(player.current.currentTime);
+        }
+    }, [playingMusic, player?.current?.currentTime]);
+
+    useEffect(() => {
+        setSongCurrentTime(0);
+        setBeforeBarStyle(0);
+    }, [nowPlaying]);
     const playPause = () => {
         if (nowPlaying) {
             player.current.onended = () => {
                 setPlayingMusic(false);
+                setSongCurrentTime(0);
+                setBeforeBarStyle(0);
+                player.current.currentTime = 0;
+                progressBar.current.value = 0;
+                cancelAnimationFrame(animationRef.current);
             };
             const currentTime = player.current.currentTime;
             if (!playingMusic) {
@@ -50,29 +75,56 @@ const MusicPlayer = ({
         }
     };
 
+    const animateProgressBar = () => {
+        progressBar.current.value = player.current.currentTime;
+        setBeforeBarStyle(player.current.currentTime);
+        animationRef.current = requestAnimationFrame(animateProgressBar);
+    };
+
+    const handleProgressBarChange = () => {
+        player.current.currentTime = progressBar.current.value;
+        setBeforeBarStyle(player.current.currentTime);
+    };
+
+    const setBeforeBarStyle = (time) => {
+        progressBar.current.style.setProperty(
+            "--seek-before-width",
+            `${(time / player.current.duration) * 100}%`
+        );
+        setSongCurrentTime(progressBar.current.value);
+    };
+
+    const handleOnLoadedMetadata = () => {
+        const duration = Math.floor(player.current.duration);
+        const formattedDuration = new Date(duration * 1000)
+            .toISOString()
+            .substr(14, 5);
+
+        setSongDuration(formattedDuration);
+        progressBar.current.max = player.current.duration;
+    };
+
+    const handleMouseDown = () => {
+        player.current.pause();
+        setPlayingMusic(false);
+        cancelAnimationFrame(animationRef.current);
+    };
+
+    const formatTime = (seconds) => {
+        const time = Math.floor(seconds);
+        const formattedTime = new Date(time * 1000).toISOString().substr(14, 5);
+
+        return formattedTime;
+    };
+
     return (
         <div>
-            {nowPlaying && (
+            {nowPlaying ? (
                 <p className="now-playing">now playing: {nowPlaying.title}</p>
+            ) : (
+                <p className="now-playing">no song selected</p>
             )}
-            <input
-                type="range"
-                id="slider"
-                className="song-slider"
-                min="0"
-                max="100"
-            />
-            {nowPlaying && (
-                <div className="song-timer">
-                    <div className="song-time-current">0</div>
-                    <p>/</p>
-                    <div className="song-time-duration">
-                        {player.current.duration}
-                    </div>
-                </div>
-            )}
-            <div className="song-controls">
-                <p>group controls: </p>
+            <div className="music-controls">
                 <SongUploader username={username} />
 
                 <label className="btn">
@@ -83,6 +135,37 @@ const MusicPlayer = ({
                         }
                     ></i>
                 </label>
+                <input
+                    type="range"
+                    className="progressBar"
+                    defaultValue="0"
+                    step="0.001"
+                    ref={progressBar}
+                    onChange={handleProgressBarChange}
+                    onMouseDown={handleMouseDown}
+                />
+            </div>
+            {nowPlaying && (
+                <div className="song-timer">
+                    <div className="song-time-current">
+                        {formatTime(player.current.currentTime)}
+                    </div>
+                    <p>/</p>
+                    <div className="song-time-duration">{songDuration}</div>
+                </div>
+            )}
+            <div className="song-controls">
+                {/* <p>group controls: </p>
+                <SongUploader username={username} />
+
+                <label className="btn">
+                    <button id="play" onClick={playPause} />
+                    <i
+                        className={
+                            playingMusic ? "fas fa-pause" : "fas fa-play"
+                        }
+                    ></i>
+                </label> */}
 
                 <audio
                     id="reactAudioPlayer"
@@ -90,6 +173,7 @@ const MusicPlayer = ({
                     src={nowPlaying && nowPlaying.url}
                     controls
                     preload="metadata"
+                    onLoadedMetadata={handleOnLoadedMetadata}
                 />
             </div>
         </div>
