7d60a8400dc25c8e0571eea9971c814dedc45b18
David Sorensen <teardownthismall@gmail.com>
2022-02-13 12:11:26 -0500

    pushing heroku, changed cors config and endpoint

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 //initializes server-side socket to persist throughout all pages, so that only one connection is needed
-const ENDPOINT = "http://localhost:4001";
+const ENDPOINT = "playlist23.herokuapp.com";
 const socket = io(ENDPOINT);
 
 export { socket };
